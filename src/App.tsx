38e0ce735e14734f2c6085c0c630709049255e71
ocabacaa <oca.locago@gmail.com>
2026-06-22 22:12:43 +0700

    feat: buat services

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
- 
+      <Services />
     </>
   );
 }
---
+++ src/data/services.ts
@@ -0,0 +1,8 @@
+export const services = [
+  "Ganti Oli",
+  "Tune Up",
+  "Servis CVT",
+  "Kelistrikan",
+  "Injeksi",
+  "Overhaul Mesin",
+];
